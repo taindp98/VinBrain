d0d1943bb499ff7946694f60ca0bb97e669f6869
taindp98 <taindp98@gmail.com>
2021-04-15 10:26:50 +0700

    apr15

--- bot.py
+++ bot.py
@@ -3,13 +3,25 @@
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
-
+import requests
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        ## custom
+        process_url = 'http://e2ebot.azurewebsites.net/api/convers-manager'
+        input_data = str(turn_context.activity.text)
+        response_object = requests.post(url=process_url, json=input_data)
+        response_object_json = response_object.json()
+
+        response_message = response_object_json['message']
+        list_mess_response = [item.replace('\n', r'').replace(r'"',r'') for item in response_message]
+
+        first_response_message = list_mess_response[0]
+
+        # await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        await turn_context.send_activity(first_response_message)
 
     async def on_members_added_activity(
         self,
