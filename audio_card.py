ee333895928909d9467676081680c6f98568c33a
taindp98 <taindp98@gmail.com>
2021-04-18 22:04:58 +0700

    apr18

--- audio_card.py
+++ audio_card.py
@@ -22,7 +22,7 @@ from botbuilder.schema import (
 def create_audio_card(audio_url_custom):
     card = AudioCard(
         # media=[MediaUrl(url="https://wavlist.com/wav/father.wav")],
-        media = [MediaUrl(url=audio_url_custom)]
+        media = [MediaUrl(url=audio_url_custom)],
         # title="I am your father",
         # subtitle="Star Wars: Episode V - The Empire Strikes Back",
         # text="The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes "
@@ -35,6 +35,7 @@ def create_audio_card(audio_url_custom):
         # image=ThumbnailUrl(
         #     url="https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg"
         # ),
+        autostart = True,
         # buttons=[
         #     CardAction(
         #         type=ActionTypes.open_url
